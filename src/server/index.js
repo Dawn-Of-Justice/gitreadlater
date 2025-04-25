ee4e495be23a3a27c79efe27615bc6532a0d5eda
Dawn-Of-Justice <salosoja@gmail.com>
2025-04-25 15:41:31 +0530

    fix: Remove trailing slash from production CORS origin URL

--- src/server/index.js
+++ src/server/index.js
@@ -20,7 +20,7 @@ const PADDLE_PUBLIC_KEY_BILLING = process.env.PADDLE_PUBLIC_KEY_BILLING;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://gitreadlater.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://gitreadlater.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
